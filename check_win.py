927075ea6a553e4d813b2214627a9629ba673b45
Ling Murong <113781784+Cris-Murong@users.noreply.github.com>
2023-03-07 13:57:50 +0800

    Update check_win.py

--- check_win.py
+++ check_win.py
@@ -1,18 +1,41 @@
 from copy import deepcopy
 
+
 class Hand:
     def __init__(self, counters: list[list[int]]) -> None:
         self.counters = counters
 
-    def is_win(self) -> bool:
-        if self._4mz_hand():
-            return True
-        if self._7d_hand():
-            return True
-        if self._13h_hand():
-            return True
-        return False
+    @classmethod
+    def create_hand_from_str(cls, hand: list[str]) -> "Hand":
+        """return a `Hand` based on the given list of string"""
+        counters_rep = [
+            [f"{i}{z}" for i in range(1, 10 if z != "z" else 8)]
+            for z in ("m", "s", "p", "z")
+        ]
+        counters = [[0 for _ in range(9)] for _ in range(3)] + [[0 for i in range(7)]]
 
+        for counter_i in range(4):
+            for rep_i in range(len(counters_rep[counter_i])):
+                counters[counter_i][rep_i] = hand.count(counters_rep[counter_i][rep_i])
+
+        return cls(counters)
+
+    def is_win(self) -> bool:
+        """check three different kind of hand to determine the winning"""
+        if self._4mz_hand():
+            print("Win: mian zi hand")
+            return True
+
+        if self._7d_hand():
+            print("Win: 7 double hand")
+            return True
+
+        if self._13h_hand():
+            print("Win: 13 head hand")
+            return True
+
+        print("Not win")
+        return False
 
     def _rule_checker(
         self,
@@ -23,54 +46,58 @@ class Hand:
         allow_s: bool,
     ) -> bool:
         """
-        check the given `rule` with the `counter`,
-        if passed - continue to call `_seb_4mz_hand`,
+        check the given `rule` with the `counter`
+
+        if passed - continue to call `_seb_4mz_hand`
+
         else - return False
 
         Parameters:
             - counter - a list of frequency for each tile
             - tile_i - index of this tile in the `counter`
             - rule - a tuple of three integer, marking the numbers of (sequence, double, triple)
-            - allow_d - allows to use rules contain double
-            - allow_s - allows to use rules contain sequence
-            
+            - allow_d - allows to use rules contain double (Y/N)
+            - allow_s - allows to use rules contain sequence (Y/N)
+
         Returns:
             - bool - results of the checking
         """
+
         s, d, t = rule  # unpack the rule (sequence, double, triple)
 
         if not allow_d and d > 0 or not allow_s and s > 0:
             return False
-        if s > 0 and tile_i > 6:
-            return False
 
-        if counter[tile_i + 1] >= s and counter[tile_i + 2] >= s:
+        if s > 0:
+            if tile_i > 6:
+                return False
+            elif counter[tile_i + 1] < s or counter[tile_i + 2] < s:
+                return False
 
-            temp_counter = deepcopy(counter)
+        temp_counter = deepcopy(counter)
 
-            temp_counter[tile_i] -= s + d * 2 + t * 3
-            temp_counter[tile_i + 1] -= s
-            temp_counter[tile_i + 2] -= s
+        temp_counter[tile_i] -= s + d * 2 + t * 3
+        temp_counter[tile_i + 1] -= s
+        temp_counter[tile_i + 2] -= s
 
-            if d:
-                allow_d = False
+        if d:
+            allow_d = False
 
-            return self._sub_4mz_hand(temp_counter, allow_d, allow_s)[0]
-
-        return False
+        return self._sub_4mz_hand(temp_counter, allow_d, allow_s)
 
     def _sub_4mz_hand(
         self, counter: list[int], allow_d: bool = True, allow_s: bool = True
-    ) -> tuple[bool]:
+    ) -> bool:
         """
         check the `counter` by calling `rule_check`
 
         Parameters:
             - counter - a list of frequency for each tile
-            - allow_d - allows to use rules contain double
-            - allow_s - allows to use rules contain sequence
+            - allow_d - allows to use rules contain double (Y/N)
+            - allow_s - allows to use rules contain sequence (Y/N)
+
         Returns:
-            - a tuple of (result, allow_double)
+            - result of the checking
         """
         if sum(counter) == 0:
             return True, allow_d
@@ -79,7 +106,7 @@ class Hand:
             if num != 0:
                 first_tile_freq, first_tile_i = num, i
                 break
-                
+
         rules = {
             1: [(1, 0, 0)],
             2: [(2, 0, 0), (0, 1, 0)],
@@ -93,22 +120,22 @@ class Hand:
                     if self._rule_checker(
                         counter, first_tile_i, rules[num][rule_i], allow_d, allow_s
                     ):
-                        return True, allow_d
-        return False, allow_d
+                        return True
+        return False
 
     def _4mz_hand(self) -> bool:
         """check 4 mian zi hand winning"""
-        allow_d = True
         res = [False for _ in range(4)]
 
         for i, counter in enumerate(self.counters):
-            res[i], allow_d = self._sub_4mz_hand(counter, allow_d, allow_s=i != 3)
-            
+            allow_d = sum(counter) % 3 != 0
+            res[i] = self._sub_4mz_hand(counter, allow_d, allow_s=i != 3)
+
         return all(res)
 
     def _7d_hand(self) -> bool:
         """check 7 doubles hand winning"""
-        double_num = 0  
+        double_num = 0  # 对子的数量
         for counter in self.counters:
             double_num += counter.count(2)
 
@@ -118,13 +145,19 @@ class Hand:
         """check 13 head hand winning"""
         for i, counter in enumerate(self.counters):
             if i == 3:
+                # 字牌
                 if not all([n not in (1, 2) for n in counter]):
                     return False
             else:
+                # 19牌
                 if counter[0] not in (1, 2) and counter[-1] not in (1, 2):
                     return False
 
         return True
 
 
-
+if __name__ == "__main__":
+    string_hand = input().split(" ")
+    hand = Hand.create_hand_from_str(string_hand)
+    print(hand.counters)
+    hand.is_win()
